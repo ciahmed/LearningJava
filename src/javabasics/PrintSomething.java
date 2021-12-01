783ed64b3eb422aea1e217477a1d3ce4fb0530b1
ciahmed <chowad@gmail.com>
2021-12-01 05:59:12 -0800

    Java Mentoring

---
+++ src/javabasics/PrintSomething.java
@@ -0,0 +1,31 @@
+package javabasics;
+
+public class PrintSomething {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java");
+        System.out.println("I am trying to print something with Java");
+
+        System.out.println("Hello World!" + " I am learning Java"); //Concatenation
+
+        System.out.print("Hello World!\n");
+        System.out.print("I am learning Java");
+
+    }
+
+    // This is for single line comment
+
+    /*
+    This
+    is
+    for
+    multi
+    line
+    comments
+     */
+
+
+
+}
---
+++ src/javabasics/Variables.java
@@ -0,0 +1,30 @@
+package javabasics;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        //Variable is placeholder for a value.
+        //Variable contains a value which act based on the data types.
+
+        /*
+        Data Types:
+        1. Primitive (8)
+        2. Non-Primitive (3)
+         */
+
+        int x; //Declaration
+        x = 123; //Assignment
+
+        int z = 456; //Initialization
+
+        int a = 100;
+        double b = 2.2;
+        boolean c = false;
+
+        System.out.println(a*b);
+
+
+
+    }
+}
